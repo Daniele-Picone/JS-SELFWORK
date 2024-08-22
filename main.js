92e14ad340730f55cc54f9a52bad7b390c23951c
Daniele Picone <danielepicone97@gmail.com>
2024-08-22 11:09:10 +0200

    primo selfwork variabili

---
+++ main.js
@@ -0,0 +1,19 @@
+// let a = 5;
+// let b = 11;
+// let c = 100;
+// // ci da il numero più grande da un elenco
+// let massimo = Math.max( a, b, c, 1000, 557, 300)
+// let minimo = Math.min( a, b, c, 1000, 557, 300 )
+//  console.log(massimo);
+//  console.log(minimo);
+
+//  let number = Math.round(5.4)
+//  console.log(number);
+//  ``
+ 
+let name = 'Pippo'
+let str = 'ciao a tutti '
+let saluto = str +  name
+console.log( saluto );
+
+ 
